4d681831846f09ec15589fd0392c0fb15a5023a7
JacksonJ01 <jaredajackson01@gmail.com>
2023-05-16 21:37:37 -0400

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from urllib.response import addinfo
-from GUI_Classes import main, os
-from dbTablesFunct import addIntoTable, createTable, clearTable, dataBases, modifyTable, selectFromTable, workoutDatabase
-
-
-if __name__ == "__main__":
-	main()
-
